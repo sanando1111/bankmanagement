4c8a2aa152652d9192c424960e74dcd77ffad305
unknown <javadevopsmc35@iiht.tech>
2020-09-30 10:51:24 +0530

    change css

--- bankmanagement-app/e2e/src/app.e2e-spec.ts
+++ bankmanagement-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bankmanagement-app app is running!');
+    expect(page.getTitleText()).toEqual('Bankmanagement-App');
   });
 
   afterEach(async () => {
